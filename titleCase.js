323dacdc55ad4b7f2fa7ae560eb973c4f5052be5
github4maninder <maninder.email@gmail.com>
2021-11-03 15:45:46 -0400

    add case file

---
+++ titleCase.js
@@ -0,0 +1,5 @@
+let example = 'pizza is alright.';
+let sample = 'drink is okay.';
+example = example.replace('alright','wonderful');
+sample = sample.replace('okay','awesome');
+console.log(example,sample);
